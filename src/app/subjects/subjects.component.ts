3eeb450a9ca2ec54fee87c7556683c2eaace6fb9
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2019-07-25 01:31:58 -0300

    subjects

---
+++ src/app/subjects/subjects.component.ts
@@ -0,0 +1,23 @@
+import { Subject } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subjects',
+  templateUrl: './subjects.component.html',
+  styleUrls: ['./subjects.component.css']
+})
+export class SubjectsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    let s: Subject<number> = new Subject<number>();
+    s.subscribe(n=>console.log(n));
+    s.next(1);
+    s.next(2);
+    s.next(3);
+    s.next(4);
+    s.complete();
+  }
+
+}
