1c14b74a6c19e4ab1866e551d33be5ee5d50b6f4
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2019-07-26 01:25:54 -0300

    creating component subject child

---
+++ src/app/subjects/subject-child/subject-child.component.ts
@@ -0,0 +1,41 @@
+import { DataModel } from './../../datamodel';
+import { Subject, Subscription } from 'rxjs';
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-subject-child',
+  templateUrl: './subject-child.component.html',
+  styleUrls: ['./subject-child.component.css']
+})
+export class SubjectChildComponent implements OnInit {
+
+  @Input() subject: Subject<DataModel>;
+  @Input() name: string;
+
+  private log: string[] = [];
+  private connected: boolean = false;
+  private subscription: Subscription;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  logData(data: DataModel) {
+    this.log.push("Timestamp: " + data.timestamp + " Data: " + data.data);
+  }
+
+  connect() {
+    this.log.push("Connected!");
+    this.connected = true;
+    this.subscription = this.subject.subscribe(
+      (data: DataModel) => {this.logData(data);},
+      (error) => {this.connected = false},
+      () => {this.connected = false; this.log.push("Finished!")}
+    );
+  }
+
+  disconnect() {
+
+  }
+}
